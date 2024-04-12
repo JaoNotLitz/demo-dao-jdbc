a2b1fb58a3252e6ba3767342fcca75bae2a836e2
JGCget <163034678+JGCget@users.noreply.github.com>
2024-04-12 17:02:55 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,7 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"Books");
         Seller seller = new Seller(1,"Pedro","pedro@gmail.com", new Date(),1200.0,obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
 
     }
