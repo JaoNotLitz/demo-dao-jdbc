4d7c62a8b8c03d9deedd9e3135aaf292ef5b0ec0
JGCget <163034678+JGCget@users.noreply.github.com>
2024-04-16 17:06:57 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,13 +6,21 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
+        System.out.println("TEST 1, FIND BY ID");
         Seller seller = sellerDao.findById(7);
         System.out.println(seller);
+        System.out.println("TEST 2, FIND BY DEPARTMENT");
+        Department department = new Department(2,null);
+        List<Seller> list = sellerDao.findByDepapartment(department);
+        for (Seller obj:list){
+            System.out.println(obj);
+        }
+
 
     }
 }
