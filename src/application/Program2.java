af371c996609e69e1ae7aa96533e369d90a5d305
JGCget <163034678+JGCget@users.noreply.github.com>
2024-04-18 16:25:04 -0300

    findAll Departments implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -5,27 +5,34 @@ import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 
+import java.util.List;
+
 public class Program2 {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
         System.out.println("Test 1, add Department");
         Department department = new Department(null,"Parking");
-        //departmentDao.insert(department);
+        departmentDao.insert(department);
         System.out.println("completed!");
 
         System.out.println("Test 2, update Department");
         Department department1 = new Department(1,"Programing");
-        //departmentDao.update(department1);
+        departmentDao.update(department1);
 
         System.out.println("Test 3, deleteById Department");
         int idToBeDeleted=7;
-       // departmentDao.deleteById(idToBeDeleted);
+        departmentDao.deleteById(idToBeDeleted);
 
         System.out.println("Test 4, selectById Department");
         int idSelected=1;
         Department dep = departmentDao.findById(idSelected);
         System.out.println(dep);
 
+        System.out.println("Test 5, selectAll Departments");
+        List<Department> departmentList = departmentDao.findAll();
+        for (int i = 0; i < departmentList.size()-1; i++) {
+            System.out.println(departmentList.get(i));
+        }
 
     }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
     private Connection conn;
@@ -118,7 +121,32 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public List<Department> findAll() {
-        return List.of();
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement(
+                    "SELECT department.* FROM department"
+            );
+            rs = st.executeQuery();
+
+            List<Department> list = new ArrayList<>();
+
+
+            while (rs.next()){
+
+                   Department dep = instantiateDepartment(rs);
+                   list.add(dep);
+            }
+            return list;
+
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
     private Department instantiateDepartment(ResultSet rs) throws SQLException {
         Department dep = new Department();
